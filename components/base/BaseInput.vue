89f926039577bc9a2c3dcfbf25237d6b46436d61
talalus <talal.suhail@gmail.com>
2020-07-19 02:53:51 +0300

    eslint BaseInput

--- components/base/BaseInput.vue
+++ components/base/BaseInput.vue
@@ -7,8 +7,8 @@
       :id="id"
       class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
       :value="value"
-      @input="$emit('input', $event.target.value)"
       v-bind="$attrs"
+      @input="$emit('input', $event.target.value)"
     />
   </div>
 </template>
